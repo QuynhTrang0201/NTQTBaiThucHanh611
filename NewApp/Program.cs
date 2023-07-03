67b84487504505c6920bdb7c08a66e4d390b17bd
NguyenThiQuynhTrang611 <changn1502@gmail.com>
2023-07-03 20:51:16 +0700

    EnterCustomer

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,8 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("PTPMQL_Nhom_01");
-Console.WriteLine("Nhap ten cua ban: ");
-string name = Console.ReadLine();
-Console.WriteLine("Nhap tuoi cua ban: ");
-string tuoi = Console.ReadLine();
-Console.WriteLine("Hoc sinh :" + name + "Tuoi :" + tuoi); 
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("PTPMQL_Nhom_01");
+//Console.WriteLine("Nhap ten cua ban: ");
+//string name = Console.ReadLine();
+//Console.WriteLine("Nhap tuoi cua ban: ");
+//string tuoi = Console.ReadLine();
+//Console.WriteLine("Hoc sinh :" + name + "," + "Tuoi :" + tuoi); 
+
+//BTVN 
+Console.WriteLine("Danh Sach Khach Hang");
+Console.WriteLine("Nhap Ten Khach Hang : ");
+string Customer_Name = Console.ReadLine();
+Console.WriteLine("Nhap dia chi :");
+string Customer_Address = Console.ReadLine();
+Console.WriteLine("Khach hang :" + Customer_Name + "," + "Dia chi :" + Customer_Address);
