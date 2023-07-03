cf17926599a403ec6c211104e8aa371a15193997
NguyenThiQuynhTrang611 <changn1502@gmail.com>
2023-07-03 17:12:40 +0700

    AddTuoi

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("PTPMQL_Nhom_01");
+Console.WriteLine("Nhap ten cua ban: ");
+string name = Console.ReadLine();
+Console.WriteLine("Nhap tuoi cua ban: ");
+string tuoi = Console.ReadLine();
+Console.WriteLine("Hoc sinh :" + name + "Tuoi :" + tuoi); 
